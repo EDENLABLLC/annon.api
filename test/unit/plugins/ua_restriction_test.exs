7ffc3cc66634a43a5470f469821106e2cb717f93
gmile <iamexile@gmail.com>
2017-09-20 17:44:14 +0300

    UA Restriction plugin: more tests

--- test/unit/plugins/ua_restriction_test.exs
+++ test/unit/plugins/ua_restriction_test.exs
@@ -122,5 +122,89 @@ defmodule Annon.Plugins.UARestrictionTest do
       conn = Conn.put_req_header(conn, "user-agent", @user_agent)
       assert conn == UARestriction.execute(conn, nil, settings)
     end
+
+    test "works correctly on different combinations", %{conn: conn} do
+      settings = %{
+        "blacklist" => [
+          %{
+            "name" => "user-agent",
+            "values" => ["Mozilla"]
+          },
+          %{
+            "name" => "content-type",
+            "values" => ["application/xml"]
+          }
+        ],
+        "whitelist" => [
+          %{
+            "name" => "user-agent",
+            "values" => ["Chrome", "Chromium"]
+          },
+          %{
+            "name" => "content-type",
+            "values" => ["application/json"]
+          },
+          %{
+            "name" => "another_rule",
+            "values" => ["a", "b", "c"]
+          }
+        ]
+      }
+
+      headers1 = [
+        {"user-agent", "Mozilla"},
+        {"content-type", "application/xml"},
+        {"something", "else"}
+      ]
+
+      headers2 = [
+        {"user-agent", "Mozilla"},
+        {"content-type", "application/json"}
+      ]
+
+      headers3 = [
+        {"user-agent", "Chrome"},
+        {"content-type", "application/xml"}
+      ]
+
+      headers4 = [
+        {"user-agent", "Chrome"},
+        {"content-type", "application/json"}
+      ]
+
+      headers5 = [
+        {"a", "x"},
+        {"b", "y"}
+      ]
+
+      headers6 = [
+        {"user-agent", "Internet Explorer"}
+      ]
+
+      Enum.each [headers1, headers2, headers3, headers6], fn headers ->
+        conn =
+          Enum.reduce headers, conn, fn {name, value}, conn ->
+            Conn.put_req_header(conn, name, value)
+          end
+
+        assert %{
+          "error" => %{
+            "message" => "You has been blocked from accessing this resource",
+            "type" => "forbidden"
+          }
+        } = conn
+        |> UARestriction.execute(nil, settings)
+        |> json_response(403)
+      end
+
+      Enum.each [headers4, headers5], fn headers ->
+        conn =
+          Enum.reduce headers, conn, fn {name, value}, conn ->
+            Conn.put_req_header(conn, name, value)
+          end
+
+        assert conn == UARestriction.execute(conn, nil, settings)
+      end
+    end
   end
 end
