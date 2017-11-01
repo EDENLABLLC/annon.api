aa21dc8ea28906ae7e8fe4261b190da7f10b937e
ybod <yuriibodarev@gmail.com>
2017-11-01 16:02:51 +0200

    [patch] updated contributors and maintainers to edenlabllc

--- mix.exs
+++ mix.exs
@@ -79,8 +79,8 @@ defmodule Annon.Mixfile do
 
   # Settings for publishing in Hex package manager:
   defp package do
-    [contributors: ["Nebo #15"],
-     maintainers: ["Nebo #15"],
+    [contributors: ["edenlabllc"],
+     maintainers: ["edenlabllc"],
      licenses: ["LISENSE.md"],
      links: %{github: "https://github.com/edenlabllc/annon.api"},
      files: ~w(lib LICENSE.md mix.exs README.md)]
